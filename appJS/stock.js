79b4ccdaf3c9cefc91ee49bf57dad660ac9bb165
Nahuel Enriquez <61632891+Trainxp@users.noreply.github.com>
2022-04-20 20:17:23 -0300

    segunda entrega js
    segunda entrega js

---
+++ appJS/stock.js
@@ -0,0 +1,12 @@
+let stockProcesadores = [
+
+{id: 1, nombre: "Ryzen 9 5950x", tipo: "procesador", cantidad: 1, desc: "procesador 16 nucleos", precio: 34000, marca: "AMD", img: '../img/RENDERRYZEN95950X.jpg'},
+{id: 2, nombre: "Ryzen 7 5800X", tipo: "procesador", cantidad: 1, desc: "procesador 12 nucleos", precio: 31100, marca: "AMD", img: '../img/RENDERRYZEN75800X.jpg'},
+{id: 3, nombre: "Ryzen 5 5600x ", tipo: "procesador", cantidad: 1, desc: "procesador 6 nucleos", precio: 31200, marca: "AMD", img: '../img/RENDERRYZEN55600X.jpg'},
+{id: 4, nombre: "Ryzen 5 3600 ", tipo: "procesador", cantidad: 1, desc: "procesador 6 nucleos", precio: 31400, marca: "AMD", img: '../img/RENDERRYZEN53600.jpg'},
+{id: 5, nombre: "Intel i9 12900k", tipo: "procesador", cantidad: 1, desc: "procesador 12 nucleos", precio: 91200, marca: "INTEL", img: '../img/RENDERINTELi912900K.jpg'},
+{id: 6, nombre: "Intel i3 10200", tipo: "procesador", cantidad: 1, desc: "procesador 6 nucleos", precio: 21500, marca: "INTEL", img: '../img/RENDERINTELi310200.jpg'},
+{id: 7, nombre: "Intel i5 10400", tipo: "procesador", cantidad: 1, desc: "procesador 6 nucleos", precio: 51500, marca: "INTEL", img: '../img/RENDERINTELi510400.jpg'},
+{id: 8, nombre: "Intel i7 10700", tipo: "procesador", cantidad: 1, desc: "procesador 8 nucleos", precio: 41500, marca: "INTEL", img: '../img/RENDERINTELi710700.jpg'},
+]
+
